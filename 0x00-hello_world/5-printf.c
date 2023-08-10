27859cdc59c507e95fe03cb43ae45f29ec78ba72
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 12:00:10 +0300

    fixing 4-puts and 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 /*print with proper grammar, but the outcome is a piece of art, */
 #include<stdio.h>
-int main ()
-{
+int main(void)
+   {
 
 printf ("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 
-}
+   }
