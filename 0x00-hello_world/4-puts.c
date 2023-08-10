dbf5425ef03a97d3b5d5f636554aaf632a1f729f
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 14:15:57 +0300

    fixing 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/*
- *  main - print output of the input in puts function
- *  return: 0 (Success)  
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
