9499292487bd2af5e28c55ecfffea7d94f84c413
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 14:10:26 +0300

    fixing 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-/*print the text*/
-int main(void){	
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-} 
+/*
+ *  main - print output of the input in puts function
+ *  return: 0 (Success)  
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
