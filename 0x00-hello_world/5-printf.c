b6a83deafa650bb18cf0132d7fb2b220169837da
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 14:24:33 +0300

    fixing 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-/*print with proper grammar, but the outcome is a piece of art, */
 #include<stdio.h>
+/**
+ * main - print the inserted input by printf function
+ * Return: 0 (Success)
+ */
 int main(void)
-   {
-
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
-   }
+}
