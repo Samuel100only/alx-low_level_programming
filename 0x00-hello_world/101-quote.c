deda4bc80d354c3da69270f6bc3c511c9cd2c3b8
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 16:33:33 +0300

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print in put to standard error
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	return (1);
+}
