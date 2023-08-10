4eab79f069f045d4589eb5519fcaacbfa5050643
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-10 11:38:16 +0300

    add files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/* print  Programming is like building a multilingual puzzle */
+#include<stdio.h>
+main()
+{	
+
+puts("\"Programming is like building a multilingual puzzle ");
+
+return(0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/*print with proper grammar, but the outcome is a piece of art, */
+#include<stdio.h>
+main ()
+{
+
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
