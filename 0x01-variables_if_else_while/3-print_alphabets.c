ce2ef517fbce26d985be011d8c741b3413aed712
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-11 12:22:10 +0300

    add 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print out the lower and uppercase alphabet
+ * return: 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	char lower_al = 'A';
+
+	while (alphabet <= 'z')
+	{
+	putchar(alphabet);
+	alphabet++;
+	}
+	while (lower_al <= 'Z')
+	{
+	putchar(lower_al);
+	lower_al++;
+	}
+	putchar('\n');
+	return (0);
+}
