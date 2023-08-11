2e9032db70b3e58854121e416dc1eed1bfe0dcb1
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-11 14:01:46 +0300

    add 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - return 0 after print all numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char nums = '0';
+
+	while (nums < '10')
+	{
+	putchar(nums);
+	nums++;
+	}
+	putchar('\n');
+	return (0);
+}
