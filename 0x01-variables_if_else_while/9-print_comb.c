8eef8e7b3ea160ff44b3b63955ab50821f17addf
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-11 15:18:39 +0300

    add file 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print out all alphabet ,except e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int alp = 0;
+
+	while (alp <= 9)
+	{
+	putchar(alp + 48);
+	putchar(',');
+	putchar(32);
+	alp++;
+	}
+	putchar('\n');
+	return (0);
+}
