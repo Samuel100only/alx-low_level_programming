fb41d71c3b5e8831000d5c182243ab951d7ed0a7
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-12 19:53:43 +0300

    adding 101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print out all alphabet ,except e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 7; x++)
+	{
+	for (y = 0; y <= 8; y++)
+	{
+	for (z = 0; z <= 9; z++)
+	{
+	if ((x < y) && (y < z))
+	{
+	putchar(x + 48);
+	putchar(y + 48);
+	putchar(z + 48);
+	if (y < 9 || x < 8 || z < 7)
+	{
+	putchar(',');
+	putchar(32);
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
