4c76c67df433ed9988bedb00187f813283640005
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 14:36:31 +0300

    add file main header and 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the lower case alpahbet 10 time
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+	for (x = 0; x <= 25; x++)
+	{
+		_putchar(x + 'a');
+	}
+	}
+	_putchar('\n');
+}
