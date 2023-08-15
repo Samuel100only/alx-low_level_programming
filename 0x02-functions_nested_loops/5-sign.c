bf77aac2dcca13592af4fa6ce2897259f5c77772
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 16:33:52 +0300

    add 5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar('+');
-	_putchar('1');
+	return ('+1');
 	}
 	else if (n == 0)
 	{
-	_putchar('0');
+	return ('0');
 	}
 	else
 	{
-	_putchar('-');
-	_putchar('1');
+	return ('-1');
 	}
 }
