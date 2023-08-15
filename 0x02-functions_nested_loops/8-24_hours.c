617ce79e8c45f5849ecb6077f6b38969779eac42
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 17:39:17 +0300

    fixing file 8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 
 	for (x = 0; x <= 2; x++)
 	{
-	for (y = 0; y <= 3; y++)
+	for (y = 0; y <= 9; y++)
 	{
-	if (( x <= 1 && y <= 9) || (x <= 2 && y <= 3))
+	if ((x <= 1 && y <= 9) || (x <= 2 && y <= 3))
 	{
 	for (z = 0; z <= 5; z++)
 	{
