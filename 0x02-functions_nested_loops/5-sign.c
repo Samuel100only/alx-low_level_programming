a4be187178064bdf8fe03342d34b2cbf90f4dcda
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 16:17:23 +0300

    add 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - if n is greater than 0 print '+' and return 1
+ * @n: the character to be checked
+ * Return: 1 or 0
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+	return (1);
+	_putchar('+');
+	}
+	else if (c == 0)
+	{
+	return (0);
+	putchar('0');
+	}
+	else
+	{
+	return (-1);
+	_putchar('-');
+}
