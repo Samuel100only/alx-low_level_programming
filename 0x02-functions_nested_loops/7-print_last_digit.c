6e4544a7d84b9a567e4cf6ee3f88128e6e80ddd3
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 17:05:55 +0300

    add file 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number c
+ * @c: the character to be checked
+ * Return: 1 or 0
+ */
+int print_last_digit(int c)
+{
+	int y;
+
+	y = c % 10;
+	return (y);
+}
