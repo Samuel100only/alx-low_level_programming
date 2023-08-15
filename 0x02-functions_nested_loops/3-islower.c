3ded7b29cecb4182eaed17d237a99766a14d311b
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-15 15:00:24 +0300

    add file 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - return 1 if c is lower case or 0 if c is upper
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	int c;
+	int y;
+
+	y = islower(c);
+	return (y);
+}
