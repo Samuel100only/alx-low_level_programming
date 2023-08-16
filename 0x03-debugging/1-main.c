0b2a0e89ba20a31808258487cb4f05fd1a6df375
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-16 13:56:07 +0300

    fixing 1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,8 +12,8 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10) /* i will never become greater than 10 */
+/*i will never become >10 need increment stament in while loop*/
+	while (i < 10)
 	{
 		putchar(i);
 	}
