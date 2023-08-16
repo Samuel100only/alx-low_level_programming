2bacac2c96aab3be210f3c5c5282267409a08e7f
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-16 12:28:59 +0300

    fixing 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,32 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
+	if (n <= 98)
 	{
 	for (; n <= 98; n++)
 	{
-		if (n == 98)
-		{
-		printf("%d", n);
-		printf("\n");
-		break;
-		}
-		else
-		printf("%d, ", n);
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	printf("%d, ", n);
 	}
 	}
-else if (n > 98)
+	else if (n > 98)
 	{
 	for (; n >= 98; n--)
 	{
 	if (n == 98)
-		{
-		 printf("%d", n);
-		 printf("\n");
-		 break;
-		}
-		else
-		printf("%d, ", n);
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	printf("%d, ", n);
 	}
 	}
 }
