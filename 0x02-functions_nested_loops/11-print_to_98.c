923e32119d957c66aaa6c0d0d552cf177e99d29f
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-16 12:23:20 +0300

    fixing 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,29 @@ void print_to_98(int n)
 if (n <= 98)
 	{
 	for (; n <= 98; n++)
-	{ 
-	if (n == 98)
+	{
+		if (n == 98)
 		{
-		printf("%d",n);
+		printf("%d", n);
 		printf("\n");
 		break;
 		}
-        	else 
-		printf("%d, ",n);
+		else
+		printf("%d, ", n);
 	}
 	}
 else if (n > 98)
 	{
 	for (; n >= 98; n--)
 	{
-	if (n == 97)
+	if (n == 98)
 		{
-		 printf("%d",n);
+		 printf("%d", n);
 		 printf("\n");
 		 break;
 		}
 		else
-		printf("%d, ",n);
+		printf("%d, ", n);
 	}
 	}
 }
