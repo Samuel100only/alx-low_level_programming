2af65d717bd6a9ab319984c781f3ff40fcb285c7
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 15:31:20 +0300

    add file 6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print - n time
+ * @n: the number of the line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+	{
+	_putchar('-');
+	}
+	_putchar('\n');
+}
