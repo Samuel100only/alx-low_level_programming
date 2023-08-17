f30b3672082cce4296faedd55d832eccc6431d9c
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 17:27:04 +0300

    add 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - print fizz buzz
+ * Return: 0
+ */
+int main()
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+	if ((x / 3 != 0)&&(x / 5 != 0))
+	{
+	printf("FizzBuzz ");
+	}
+	if (x / 3 != 0)
+	{	
+	printf("Fizz ");
+	}
+	if (x / 5 != 0)
+	{
+	printf("Buzz ");
+	}
+	printf("%d ",x);
+	}
+	printf('\n');
+}
