7b5009ff3d9a9ad25a75f83cfd7f1f810b5233cf
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 14:13:09 +0300

    add file 3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - return number from 0 - 9 and newline chr
+ * Return: 0 is sucess
+ */
+void print_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+	_putchar(y + '0');
+	}
+}
