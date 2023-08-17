0c4d3dff0cde4b7f7b8603fecf627884a2dbfabe
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 14:20:33 +0300

    add file 4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - return number from 0 - 9 and newline chr
+ * Return: 0 is sucess
+ */
+void print_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		if (c != 2 || c != 4)
+		{
+		_putchar(y + '0');
+		}
+	}
+	_putchar('\n');
+}
