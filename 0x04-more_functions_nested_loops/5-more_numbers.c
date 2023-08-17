3606767805d5c53f18f8391fc8480a0e03f2ae8f
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 15:10:14 +0300

    add file 5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - return 0 to 14 ten times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+		if (y >= 0 || y <= 9)
+		{
+		_putchar(y + '0');
+		}
+		else
+		{
+		_putchar('1');
+		_putchar('0' + (y % 10));
+		}
+	}
+	_putchar('\n');
+	}
+}
