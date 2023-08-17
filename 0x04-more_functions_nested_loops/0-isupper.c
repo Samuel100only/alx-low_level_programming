ef23ce8fc20ece2aa3d3e47a4cda97c7cddd853c
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 13:38:19 +0300

    add files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - return 1 if c is uppercase otherwise return 0
+ * @c: the character to be checked
+ * Return: 1 for uppercase 0 for lower
+ */
+int _isupper(int c)
+{
+	if (c >= 65 || c <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
