044bf307a87895a4022cc5803b2c621de81e751c
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-17 14:06:56 +0300

    add file 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multyply and return the result
+ * @a: the character to be multiply
+ * @b: the character to be multiply
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
