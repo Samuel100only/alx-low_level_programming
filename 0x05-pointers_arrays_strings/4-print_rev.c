143b7aeebf07999ca759ae2740dcc6c9f651aa22
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-21 15:07:07 +0300

    add file 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - print the value in reverse order
+ * @s: the string variable
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\n'; x++)
+	{}
+	for (x; s[x] >= 0; x--)
+	{
+	_putchar(s[x]);
+	}
+	_putchar('\n');
+}
