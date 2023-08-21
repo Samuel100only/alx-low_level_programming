5e6b24e52d7ae4e98dbf39c9862af4fab070638f
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-21 18:38:28 +0300

    add file 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of the string
+ * @str: the variable of string of char
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int y;
+	int z;
+
+	for (y = 0; str[y] != '\0'; y++)
+	{}
+	if (y % 2 == 0)
+	{
+	y = y / 2;
+	}
+	else
+	{
+	y = (y - 1) / 2;
+	}
+	for (z = y; str[z] != '\0'; z++)
+	{
+	}
+	_putchar('\n');
+}
