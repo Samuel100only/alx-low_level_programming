4864afd187122b3a3d228e300ee43dd93d20f33d
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-22 19:30:42 +0300

    add file 100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - convert the string to interger
+ * @s: the string variable
+ * Return: value of int s
+ */
+int _atoi(char *s)
+{
+	int x;
+	int w;
+	
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+		if ( (*(s + x) = '-') && (57 >= *(s + x + 1) <= 48)
+		{
+		for (; 57 >= *(s + x + 1) <= 48; x++)
+		{
+		
+		}
+		}
+	}
+}
