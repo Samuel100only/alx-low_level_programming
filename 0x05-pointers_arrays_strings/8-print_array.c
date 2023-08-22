6eea855f45bb59643e570db6eb5818b948eac957
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-22 16:21:35 +0300

    add file 8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array - print the valua of the array a
+ * @a: the array variable
+ * @n: the number of array
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+	printf("%d, ", *(a + x));
+	}
+}
