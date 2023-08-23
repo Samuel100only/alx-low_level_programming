9543c749c30d56cf4eccb0a4161d3f501d919348
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-23 11:56:33 +0300

    add file 0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - append the src string to the dest string
+ * @dest: the pointer variable to char
+ * @src: the pointer variable to char
+ * Return: the output to the pointer dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (*(dest + x) != '\0')
+	{
+	x++
+	}
+	x = x + 2;
+	for (y = 0;*(src + y) != '\0'; y++)
+	{
+	*(dest + x) = *(src + y);
+	x++;
+	}
+	return (dest);
+}
