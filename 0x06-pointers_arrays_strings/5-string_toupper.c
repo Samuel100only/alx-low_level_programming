66a63a9a74e8c3eeb3d99248a8944cddce19dc23
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-24 11:46:18 +0300

    add file 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change strings all lowercase to upper
+ * str: the string to be change
+ * Return: the adress of str
+ */
+char *string_toupper(char *str)
+{
+	int x;
+
+	for (x = 0; *(str + x); x++)
+	{
+	if ((*(str + x) >= 97) && (*(str + x) <= 122))
+	{
+	*(str + x) = *(str + x) - 32;
+	}
+	}
+	return (str);
+}
