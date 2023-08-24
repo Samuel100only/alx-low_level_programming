986a1f5ffa3abea61d58bbb666fbcff114ac8c8d
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-24 21:51:17 +0300

    add file 100_rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encode string using r0t13
+ * @s: the string to be encodeded
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	char input[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char output[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int x;
+	int y;
+
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+	for (y = 0; y < 52; y++)
+	{
+	if (*(s + x) == input[y])
+		{
+		*(s + x) = output[y];
+		}
+	}
+	}
+	return (s);
+}
