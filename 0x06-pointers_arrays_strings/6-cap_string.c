77fea82ddfec4acc3cae375159f46b3be90a7da6
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-24 14:04:48 +0300

    add file 6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * cap_string - change the first char of word to uppercase
+ * @srt: the string variable
+ * Return: address of srt
+ */
+char *cap_string(char *srt)
+{
+	int x;
+	for (x = 0; *(srt + x); x++)
+	{
+	if (*(srt + x) >= 32 && *(srt + x) <= 63 && *(srt + x) != '\t' )
+	{
+	if (*(srt + x + 1) >= 97 && *(srt + x + 1) <= 122)
+	{
+	*(srt + x + 1) = *(srt + x + 1) - 32;
+	}
+	}
+	}
+	return (srt);
+}
