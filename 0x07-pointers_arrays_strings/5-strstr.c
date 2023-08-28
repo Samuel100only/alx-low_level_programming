c64f201b600a350a16fa2987ebaeb23f549a692c
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-28 18:04:38 +0300

    add file 5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - return the pointer of first occurrence of the sub-st
+ * @haystack: the sub-string to find in
+ * @needle: the string to find
+ * Return: the address of the s first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x, y;
+
+	for (x = 0; *(haystack + x) != '\0'; x++)
+	{
+	for (y = 0; *(needle + y) != '\0'; y++)
+	{
+	if (*(haystack + x) == *(needle + y))
+	if (*(haystack + x + 1) == *(needle + y + 1))
+	{
+	return (haystack + x);
+	}
+	}
+	}
+	return ('\0');
+}
