60203ccc533a26ed1f42fa93fe4046ad4665e36a
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-30 14:15:04 +0300

    fixing 0-puts_recur

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - print string of character followed by newline
- * @s - the string variable
+ * @s: the string variable
  * Return: void
  */
 void _puts_recursion(char *s)
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 	_putchar(*s);
-        _puts_recursion(s + 1);
+	_puts_recursion(s + 1);
 	}
 	if (*s == '\0')
 	{
