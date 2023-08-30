6063bde6028b6c76bab9304d4d85906edb78d9da
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-08-30 16:10:53 +0300

    add file 4-pow_recu

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,18 +2,18 @@
 /**
  * _pow_recursion - return the power of x to y
  * @x: the power
- * @y; power to
+ * @y: power to
  * Return: result
  */
 int _pow_recursion(int x, int y)
 {
 	if (y == 0)
-        {
-        return (1);
-        }
-        else if (y < 0)
-        {
-        return (-1);
-        }
+	{
+	return (1);
+	}
+	else if (y < 0)
+	{
+	return (-1);
+	}
 	return (x * _pow_recursion(x, y - 1));
 }
