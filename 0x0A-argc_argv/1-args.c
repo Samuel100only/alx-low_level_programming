ee4312d8a0f077355f5ae50010096600308785f8
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-01 17:36:51 +0300

    add file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the arrgument from commandline
+ * @argc: its the size of the argument strings
+ * @argv: its the array where string insert
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+	return (0);
+}
