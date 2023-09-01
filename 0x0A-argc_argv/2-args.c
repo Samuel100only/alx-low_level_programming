5779d4cf2965051d6bb396fac746f0c6eff64df4
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-01 17:41:53 +0300

    add file 2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the arrgument from commandline
+ * @argc: its the size of the argument strings
+ * @argv: its the array where string insert
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
