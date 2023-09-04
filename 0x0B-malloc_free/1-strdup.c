dddf854e8ec2f06828a9dcdeffd8dbc4580094eb
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-04 14:31:45 +0300

    add file 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strdup - return a pointer to a newly allocated space in memory with string
+ * @str: the string
+ * Return: the pointer address
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int x,y;
+
+	x = 0;
+	while(*(str + x) != '\0')
+	{
+	x++;
+	}
+	p = (char *)malloc(sizeof(char) * x);
+	if (p == NULL || x == 0)
+	{
+	return (NULL);
+	}
+	for (y = 0; *(p + y) <= x; y++)
+	{
+	*(p + y) = *(str + y);
+	}
+	return (p);
+}
