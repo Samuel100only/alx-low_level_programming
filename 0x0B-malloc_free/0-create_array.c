b3daba7b6e61ae360a172f9283c3eb179c662370
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-04 13:52:04 +0300

    add file 0-create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - create an array of chars and initialize it with char
+ * @size: size of the array
+ * @c: the character the array fill by
+ * Return: the adress of the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int x;
+
+	p = malloc(sizeof(char) * size);
+	if (p == NULL || size == 0)
+	{
+	return ("NULL");
+	}
+	for (x = 0; x < size; x++)
+	{
+	*(p + x) = c;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#0x0B-malloc_free
+0-create_array.c: function that creates an array of chars, and initializes it with a specific char
