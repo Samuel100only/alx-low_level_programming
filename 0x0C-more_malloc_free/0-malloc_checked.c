e17e9313650c277a013cdd65361953106e730551
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-07 10:50:42 +0300

    add file 0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the size of memory
+ * Return: the pointer of allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	
+	p = malloc(b);
+	if (p == NULL)
+	{
+	exit (98);
+	}
+	return (p);
+}
