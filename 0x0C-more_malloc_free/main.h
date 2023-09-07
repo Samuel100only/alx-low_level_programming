d64f3799e14f168c2fadfde378061a7d8e76a0bd
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-07 15:34:59 +0300

    add main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,4 +3,7 @@
 #include <stdlib.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 #endif
