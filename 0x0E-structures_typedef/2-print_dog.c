b971195b476ff78269d56401fe3d4ce3831222d3
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-11 21:51:19 +0300

    add file 2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 /**
  * print_dog -  prints a struct dog
@@ -9,15 +9,12 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-	exit;
+	return;
 	}
 	if ((*d).name == NULL)
 	{
 	(*d).name = "(nil)";
 	}
-	if ((*d).age == 0)
-	{
-	(*d).age = "(nil)";
 	if ((*d).owner == NULL)
 	{
 	(*d).owner = "(nil)";
