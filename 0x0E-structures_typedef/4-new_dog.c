dccd814a3593f330c94bec88aabaa6e1493f1db8
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-11 22:23:48 +0300

    fixing file 4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,32 +13,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p;
 	int len1, len2;
 
-	if (p == NULL)
-	{
-	return (NULL);
-	}
 	len1 = strlen(name);
 	len2 = strlen(owner);
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
 
-	p.name = malloc(sizeof(char) * (len1 + 1));
-	if (p.name == NULL)
+	p->name = malloc(sizeof(char) * (len1 + 1));
+	if (p->name == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
-	p.owner = malloc(sizeof(char) * (len2 + 1));
-	if (p.owner == NULL)
+	p->owner = malloc(sizeof(char) * (len2 + 1));
+	if (p->owner == NULL)
 	{
 		free(p);
-		free(p.name);
+		free(p->name);
 		return (NULL);
 	}
-	strcpy(p.name, name);
-	strcpy(p.owner, owner);
-	dog.age = age;
+	strcpy(p->name, name);
+	strcpy(p->owner, owner);
+	p->age = age;
 
 	return (p);
 }
