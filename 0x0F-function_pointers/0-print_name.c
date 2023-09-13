8813ad020a0381666da50f8900ff81135930e777
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-13 13:59:14 +0300

    add file 0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - sent the string name to be print by function f
+ * @name: the string to be pass on to f
+ * @f: the function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+#0x0F-function_pointers
+0-print_name - print a name
