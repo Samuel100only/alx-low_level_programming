5bfff8ddeb0d7a671026bd1d84763f78fed12a40
root <root@LAPTOP-H0OJH0RF.localdomain>
2023-09-13 15:37:03 +0300

    add file 2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - returns the index of the first element
+ * for which the cmp function does not return 0
+ * @array: the array pointer
+ * @size: the size of the array
+ * @cmp: the pointer to function
+ * Return: the integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (size <= 0)
+	{
+	return (-1);
+	}
+	for (x = 0; x < size; x++)
+	{
+	cmp(array[x]);
+	if (cmp(array[x]) != 0)
+	{
+	return (x);
+	}
+	}
+	return (-1);
+}
